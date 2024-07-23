7eeab49c346fdbb73693c435ba767e8aa46185af
Roshini <104883342+vu21csen0300061@users.noreply.github.com>
2024-07-23 17:24:46 +0530

    Add files via upload

---
+++ encodegenerator.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import face_recognition
+import pickle
+import face_recognition_models
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,
+                        {'databaseURL': "https://realtimefaceattendence-9c5cd-default-rtdb.firebaseio.com/",
+                         'storageBucket':"realtimefaceattendence-9c5cd.appspot.com"})
+
+folderModePath = 'Images'
+modePathList = os.listdir(folderModePath)
+imgList = []
+studentIds=[]
+for path in modePathList:
+    imgList.append(cv2.imread(os.path.join(folderModePath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+    fileName = f'{folderModePath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+def findEncodings(imagesList):
+    encodeList = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+print("Encoding Started ...")
+encodeListKnown = findEncodings(imgList)
+encodeListKnownWithIds = [encodeListKnown, studentIds]
+print("Encoding Complete")
+
+file = open("EncodeFile.p", 'wb')
+pickle.dump(encodeListKnownWithIds, file)
+file.close()
+print("File Saved")
